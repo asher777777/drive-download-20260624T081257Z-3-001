77605a1dcb5ac7a9feb52460b44b5195641b0095
asher777777 <ovt5771@gmail.com>
2026-09-06 22:03:42 +0300

    add next.config.js

---
+++ next.config.js
@@ -0,0 +1,42 @@
+﻿/** @type {import('next').NextConfig} */
+const nextConfig = {
+  serverExternalPackages: ["firebase-admin", "@google-cloud/text-to-speech", "puppeteer"],
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "10mb",
+      allowedOrigins: [
+        "hakel.club",
+        "www.hakel.club",
+        "c-g-ltd.web.app",
+        "c-g-ltd.firebaseapp.com",
+        "localhost:3000",
+        "127.0.0.1:3000"
+      ]
+    }
+  },
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "storage.googleapis.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
